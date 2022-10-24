0345cd3948e7b532ba5582f37ff84aec5bd9e249
ItsYaBoiNeil <115005329+ItsYaBoiNeil@users.noreply.github.com>
2022-10-24 11:41:42 +0530

    linked pages and made the home page sexier

--- lookmyshow/login.php
+++ lookmyshow/login.php
@@ -75,19 +75,21 @@ if(empty($err))
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>PHP login system!</title>
+    <link rel="stylesheet" href="style.css" 
+    <title></title>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Php Login System</a>
+  <nav class="navbar navbar-dark" style="background-color: #800000;">
+  <a class="navbar-brand" href="#">LookMyShow Login</a>
+  <img class="bg" src="movies.jpg" alt="background">
+  <img class="logo" src="logo.png" alt="logo">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
   <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="register.php">Register</a>
